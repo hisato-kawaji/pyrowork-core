954568a359d9a93c291d572698d234228c23413a
hisato-kawaji <hkawaji@moff.mobi>
2017-08-03 19:42:37 +0900

    added error-handling scope and config class

--- app/functions/sample/default_function.py
+++ app/functions/sample/default_function.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import boto3
 import sys
 import os
 
 
-def get_by_id(event, context):
+class Config:
+    _instance = None
 
-    try:
-        print("execute")
-    except Exception as e:
-        print("type:{0}".format(type(e)))
-        print("args:{0}".format(e.args))
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        print("Error Detail:", sys.exc_info()[1])
-        raise
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Config, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        for key, value in os.environ.items():
+            setattr(self, key, value)
+
+    def get(key):
+        return getattr(self, key)
+
+
+class Executor:
+
+    def run(function, *args):
+        try:
+            ret = function(*args)
+            return ret
+        except Exception as e:
+            print("type:{0}".format(type(e)))
+            print("args:{0}".format(e.args))
+            print('Error poi')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
+
+
+executor = Executor
+
+
+def get_by_id(event, context):
+    def main(event, context):
+        raise Exception("error!")
+        print('hogefuga-')
+
+    executor.run(main, event, context)
+
     return 'get_by_id'
 
 
@@ -73,5 +105,6 @@ def delete(event, context):
 
 
 if __name__ == '__main__':
+    get_by_id(None, None)
     print('Cannot execute lambda functions directory. Try to use test console command')
 
