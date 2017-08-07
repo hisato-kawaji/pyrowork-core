dbe35a3b7970973fd2cea3e217f1f0cdd7426a7b
hisato-kawaji <hkawaji@moff.mobi>
2017-08-07 11:19:46 +0900

    added functions

---
+++ app/functions/balances/default_function.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import boto3
+import sys
+import os
+from framework import Config, Executor
+from framework import Exceptions as ex
+
+
+def get_by_id(event, context):
+    def main(event, context):
+
+    return Executor.run(main, event, context)
+
+
+def get_all(event, context):
+    def main(event, context):
+
+    return Executor.run(main, event, context)
+
+
+def create(event, context):
+    def main(event, context):
+
+    return Executor.run(main, event, context)
+
+
+def update(event, context):
+    def main(event, context):
+
+    return Executor.run(main, event, context)
+
+
+def delete(event, context):
+    def main(event, context):
+
+    return Executor.run(main, event, context)
+
+
+if __name__ == '__main__':
+    print('Cannot execute lambda functions directory. Try to use test console command')
+
