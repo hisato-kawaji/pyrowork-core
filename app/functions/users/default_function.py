aead618395cac901d9675f60b9b7f954a18280c0
hisato-kawaji <hkawaji@moff.mobi>
2017-09-05 15:44:06 +0900

    fix bug

--- app/functions/users/default_function.py
+++ app/functions/users/default_function.py
@@ -49,10 +49,8 @@ def get_by_institution_id(event, context):
                 '%s:institution_id=%s is not found' % (Config().table_name, event['path']['id'])
             )
 
-        response = table.batch_get_item(RequestItems={
-            Config().table_name: {
-                'Keys': user_list
-            }
+        response = table.get_item(RequestItems={
+            'Keys': user_list
         })
 
         return response['Responses'][Config().table_name]
