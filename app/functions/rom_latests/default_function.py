9f8ef961413e68f44fefc6709b0460d09eef35e7
hisato-kawaji <hkawaji@moff.mobi>
2017-11-22 11:39:06 +0900

    change column definition for getting list of user's rom latests

--- app/functions/rom_latests/default_function.py
+++ app/functions/rom_latests/default_function.py
@@ -7,6 +7,7 @@ import decimal
 import boto3
 from framework import Config, Executor
 from framework import Exceptions as ex
+from boto3.dynamodb.conditions import Key
 
 
 def get_by_id(event, context):
@@ -15,8 +16,8 @@ def get_by_id(event, context):
         table = dynamodb.Table(Config().table_name)
         response = table.get_item(
             Key={
-                'user_position_id': event['path']['user_id'],
-                'item_id': decimal.Decimal(event['path']['item_id'])
+                'user_id': event['path']['user_id'],
+                'item_position_id': event['path']['item_id']
             }
         )
 
@@ -30,34 +31,48 @@ def get_by_id(event, context):
     return Executor.run(main, event, context)
 
 
+def get_by_user_id(event, context):
+    def main(event, context):
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(Config().table_name)
+        response = table.query(
+            KeyConditionExpression=Key('user_id').eq(event['path']['id'])
+        )
+
+        if not response.get('Items'):
+            raise ex.NoRecordsException(
+                '%s:%s is not found' % (Config().table_name, event['path']['user_id'])
+            )
+
+        return response['Items']
+
+    return Executor.run(main, event, context)
+
+
 def save(event, context):
     def main(event, context):
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(Config().table_name)
 
         duplicate_key = {
-            'user_position_id': event['body']['user_position_id'],
-            'item_id': decimal.Decimal(event['body']['item_id'])
+            'user_id': event['body']['user_id'],
+            'item_position_id': decimal.Decimal(event['body']['item_id'])
         }
         latest_record = table.get_item(Key=duplicate_key)
 
         if 'Item' not in latest_record:
             latest_record.update(event['body'])
-            # TODO:event['body']に入ってくる指定のカラムのみをDecimalに変換する仕組みを作る
-            latest_record['item_id'] = decimal.Decimal(latest_record['item_id'])
             latest_record['updated_at'] = Config().now()
 
         else:
             latest_record = {
-                'user_position_id': None,
-                'item_id': None,
+                'user_id': None,
+                'item_position_id': None,
                 'started_at': None,
                 'created_at': Config().now(),
                 'updated_at': Config().now()
             }
             latest_record.update(event['body'])
-            # TODO:event['body']に入ってくる指定のカラムのみをDecimalに変換する仕組みを作る
-            latest_record['item_id'] = decimal.Decimal(latest_record['item_id'])
         response = table.put_item(Item=latest_record)
 
         return response
